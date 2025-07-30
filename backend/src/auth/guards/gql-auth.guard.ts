c673e732771ccf4d0599d612ec5406cd335c7bd2
Andrii Dryha <driga.and@gmail.com>
2025-07-30 15:31:54 +0300

    delete comment

--- backend/src/auth/guards/gql-auth.guard.ts
+++ backend/src/auth/guards/gql-auth.guard.ts
@@ -6,6 +6,6 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 export class GqlAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req; // для стратегии используем req из GraphQL-контекста
+    return ctx.getContext().req;
   }
 }
