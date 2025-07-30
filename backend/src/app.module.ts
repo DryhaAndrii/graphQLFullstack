07303d603137df81972c2e52fc39bad28cc2e5c1
Andrii Dryha <driga.and@gmail.com>
2025-07-30 11:11:44 +0300

    initialized backend with Nest.js, Graph.ql, Apollo, Prisma. And also with users table migration

---
+++ backend/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { HelloResolver } from './hello/hello.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+  ],
+  controllers: [],
+  providers: [HelloResolver],
+})
+export class AppModule {}
